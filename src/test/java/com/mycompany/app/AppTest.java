2645c2b5254510db8545a3534b33d59643273a02
nagenn <44678680+nagenn@users.noreply.github.com>
2020-11-22 15:06:53 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,7 +34,6 @@ public class AppTest
     {
         App.main(null);
         try {
-            System.out.println(outContent.toString());
             assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World! \"");

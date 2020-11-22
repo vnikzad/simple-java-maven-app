792e9716ddd4bf57eecb6d7bb2584a0ad003df1d
nagenn <44678680+nagenn@users.noreply.github.com>
2020-11-22 15:10:46 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -44,8 +44,9 @@ public class AppTest
     public void testAppCalc()
     {
         try {
-            int res=App.calc();
-            assertEquals(50, res);
+            int res=App.calc(5,10);
+            int expected=5*10;
+            assertEquals(expected, res);
         } catch (AssertionError e1) {
             fail("Not 50");
         }
